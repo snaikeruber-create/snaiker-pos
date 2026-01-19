d2d12f762a6cd3f807742b6a17f0c17b1284524a
snaikeruber-create <snaikeruber@gmail.com>
2026-01-20 00:27:19 +0100

    Fix Railway build (start script + PORT)

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).send("Snaiker POS is running");
-});
+app.use(bodyParser.json());
 
 app.post("/webhooks/uber", (req, res) => {
   console.log("Webhook reçu:", req.body);
   res.status(200).json({ received: true });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.get("/", (req, res) => {
+  res.send("Snaiker POS running");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
   console.log(`Snaiker POS running on port ${PORT}`);
 });
